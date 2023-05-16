7ee166e10860b023cfbe84dd10a5df2580a192ae
user23007 <115121523+user23007@users.noreply.github.com>
2023-05-16 16:51:07 -0500

    Update Compilador.py
    Just updated the input, try this input here to see how it works:
    S → AbC
    A→dfo

--- Compilador.py
+++ Compilador.py
@@ -1,23 +1,35 @@
 import string
 ##Código
-gramatica = {
-    "<S>": ["<LETRA>", "<LETRA><S>"],
-    "<LETRA>": list(string.ascii_lowercase + string.ascii_digits) #alfabeto en minuscula
-}
 
-def verificar_string(cadena):
-    S → aSb | c F → 
-
-# Pedir la gramática
-gramatica = input("Ingrese su gramática: ")
-
+def First(cadena):
+    print("hola")
+    
 # Pedir los strings hasta que se ingrese una cadena vacía
+diccionario = {}
 cadena = []
 while True:
-    string = input("Ingrese un string para la gramática (deje en blanco para finalizar): ")
-    if not string:
+    gramatica = input("Ingrese un string para la gramática (deje en blanco para finalizar): ")
+    new_gramatica = gramatica.replace(" ", "")
+    if not gramatica:
         break
-    cadena.append(string)
+    cadena_aux = list(new_gramatica)
+    cadena.append(cadena_aux)
 
-print("La gramática ingresada es:", gramatica)
-print("Los strings ingresados son:", cadena)
+
+for i in cadena:
+    while ' ' in cadena:
+        cadena.remove(' ')
+
+#diccionario[clave] = valor
+for i in range(len(cadena)):
+    j = 0
+    diccionario[cadena[i][0]] = cadena[i][2:]
+    j = j+1
+
+
+print("La gramática ingresada es: ", cadena)
+print("La gramática ingresada es: ", diccionario)
+
+
+#Analisis -> Para candena[i] siempre será 'S' = 0, '→' = 1, otro = 2+
+
