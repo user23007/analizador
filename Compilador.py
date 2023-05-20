0ee3cc142802a5da99140cb7851663ecc7b18e4e
user23007 <115121523+user23007@users.noreply.github.com>
2023-05-20 11:57:46 -0500

    Update Compilador.py
    simple first not completed

--- Compilador.py
+++ Compilador.py
@@ -1,12 +1,13 @@
 import string
 ##Código
 
-def First(cadena):
-    print("hola")
     
 # Pedir los strings hasta que se ingrese una cadena vacía
+diccionario_first = {}
+diccionario_follow = {}
 diccionario = {}
 cadena = []
+sub_cadena = []
 while True:
     gramatica = input("Ingrese un string para la gramática (deje en blanco para finalizar): ")
     new_gramatica = gramatica.replace(" ", "")
@@ -19,17 +20,37 @@ while True:
 for i in cadena:
     while ' ' in cadena:
         cadena.remove(' ')
+#print(cadena[0][0], " ", cadena[1][0])
 
-#diccionario[clave] = valor
 for i in range(len(cadena)):
-    j = 0
-    diccionario[cadena[i][0]] = cadena[i][2:]
-    j = j+1
+    clave = cadena[i][0]
+    if clave in diccionario:
+        if diccionario[clave] != cadena[i][2:]:
+            diccionario[clave + '1'] = cadena[i][2:]
+    else:
+        diccionario[clave] = cadena[i][2:]
+#print(diccionario)
 
+lista_caracteres = list(string.digits + string.ascii_lowercase + string.punctuation)
+epsilom = "#"
 
-print("La gramática ingresada es: ", cadena)
-print("La gramática ingresada es: ", diccionario)
+def get_first(diccionario):
+    for clave in diccionario.keys():
+        for i in range(len(lista_caracteres)):
+            if(lista_caracteres[i] in diccionario[clave][0] or epsilom in diccionario[clave][0]):
+                diccionario_first[clave] = diccionario[clave][0]
+
+    for clave in diccionario_first.keys():
+        print("en proceso")
+
+    
+        
+
+get_first(diccionario)
+print(diccionario_first)
 
 
 #Analisis -> Para candena[i] siempre será 'S' = 0, '→' = 1, otro = 2+
+
+#diccionario[clave] = valor
 
