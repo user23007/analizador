fde10e44ac5b2f75d40be48b325134337b6f962e
user23007 <115121523+user23007@users.noreply.github.com>
2023-05-22 13:29:04 -0500

    Update Compilador.py

--- Compilador.py
+++ Compilador.py
@@ -8,7 +8,6 @@ diccionario_follow = {}
 diccionario = {}
 cadena = []
 sub_cadena = []
-#manera de pedir el string:   A > x   ó    A : x
 while True:
     gramatica = input("Ingrese un string para la gramática (deje en blanco para finalizar): ")
     new_gramatica = gramatica.replace(" ", "") #Quitamos todos los espacios que tenga la cadena del usuario
@@ -20,43 +19,56 @@ while True:
 
 for i in cadena:
     while ' ' in cadena:
-        cadena.remove(' ') #removemos espacios adicionales si es que el usuario ingresa A >  ebc (acá hay dos espacios) 
+        cadena.remove(' ') #removemos espacios adicionales si es que el usuario ingresa A ->  ebc 
 #print(cadena[0][0], " ", cadena[1][0])  #como verás, podemos acceder a la cadena del usuario, que esta en una lista,
 # ejemplo: [[a,b,c],[d,e,f]] aqui cadena[0][0] sería a, y cadena[1][2] sería f
 
 
 #Meteremos la cadena en un diccionario
+aumentador_not_used = 0
+lista_inicio_not_used = ['1','2','3','4','5','6','7','8','9']
 for i in range(len(cadena)):
     clave = cadena[i][0] #decimos que la clave del diccionario será el primer item de cada lista, ya que el primero siempre es no terminal
     if clave in diccionario: #aqui clave toma el valor de el diccionario
         if diccionario[clave] != cadena[i][2:]: #recordar que las claves son unicas y no se pueden repetir, esto es un problema para el or 
-            diccionario[clave + '1'] = cadena[i][2:] #Aqui agregamos un 1 a cualquier clave que se repita, si agregas A > bcd    A > e, entonces se veria como A y A1
+            
+            diccionario[clave + lista_inicio_not_used[aumentador_not_used]] = cadena[i][2:] #Aqui agregamos un 1 a cualquier clave que se repita, si agregas A > bcd    A > e, entonces se veria como A y A1
+            aumentador_not_used = aumentador_not_used + 1
     else:
         diccionario[clave] = cadena[i][2:] #Si no se repite ( o sea, si no hay un or) entonces se agrega normal
 #print(diccionario)
 
-lista_caracteres = list(string.digits + string.ascii_lowercase + string.punctuation) #esta es la lista de TERMINALES
+lista_terminales = list(string.digits + string.ascii_lowercase + string.punctuation) #esta es la lista de TERMINALES
+lista_noTerminales = list(string.ascii_uppercase)
 epsilom = "#"
 
 def get_first(diccionario):
     for clave in diccionario.keys(): #aqui clave toma el valor de las claves del diccionario
-        for i in range(len(lista_caracteres)):
-            if(lista_caracteres[i] in diccionario[clave][0] or epsilom in diccionario[clave][0]): #Aqui, si algun caracter esta de primero en el diccionario (en los valores)
-                diccionario_first[clave] = diccionario[clave][0]#Entonces metemos a ese caracter en el diccionario de first
+        for i in range(len(lista_terminales)):
+            if(len(clave) == 1):
+                if(lista_terminales[i] in diccionario[clave][0] or epsilom in diccionario[clave][0]): #Aqui, si algun caracter esta de primero en el diccionario (en los valores)
+                    diccionario_first[clave] = diccionario[clave][0]#Entonces metemos a ese caracter en el diccionario de first
+                    
+            elif(len(clave) == 2):
+                clave_first = clave[:-1]
+                if diccionario_first.get(clave, False) is True:
+                    if(lista_terminales[i] in diccionario[clave_first][0] or epsilom in diccionario[clave_first][0]):
+                        #if diccionario[clave_first]:
+                        print("Pase")
+                        diccionario_first[clave_first] += diccionario[clave][0]
+                else:
+                    if(lista_terminales[i] in diccionario[clave][0] or epsilom in diccionario[clave][0]):
+                            print("Pase")
+                            diccionario_first[clave_first] = diccionario_first.get(clave_first, "") + diccionario[clave][0]
+            
+        for i in range(len(lista_noTerminales)):
+            #########YA PODEMOS CONTINUAR ACÁ##################
 
-    for clave in diccionario_first.keys(): #Aqui la idea es meter los first de los strings de la forma noTerminal > noTerminal, ejemplo: A -> Ba  B -> e
-        #recuerda que en el ejemplo anterior, ya habriamos metido B -> e en el diccionario de first, eso será una ayuda en este caso
-        print("Completar")
-        
+                
 
     
         
 
 get_first(diccionario)
+print(diccionario)
 print(diccionario_first)
-
-
-#Analisis -> Para candena[i] siempre será 'S' = 0, '→' = 1, otro = 2+
-
-#diccionario[clave] = valor
-
